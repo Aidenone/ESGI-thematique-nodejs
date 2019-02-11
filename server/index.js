5775dda69edacc1a9e8b6628994e6da9b3bd6e75
Antoine LUCAS <antoinelucas@MacBook-Pro-de-Antoine-2.local>
2019-02-11 17:23:41 +0100

    fin premier cours

--- server/index.js
+++ server/index.js
@@ -10,4 +10,32 @@ mongoose.connect('mongodb://mongo' , {
 const db = mongoose.connection;
 
 db.on('error', function () {console.log(arguments)});
-db.on('open', function () {console.log("Connected")});
+db.on('open', function () {
+    const Schema = mongoose.Schema;
+
+    const movieDetailsSchema = new Schema({
+        title: String,
+        year: {type: Number, min:1850},
+        released: Date
+    });
+
+    const Movie = mongoose.model('Movie', movieDetailsSchema);
+
+    const movie1 = new Movie();
+    movie1.title = "Test Movie";
+    movie1.year = 1930;
+    movie1.released = Date.now();
+
+    movie1.save(function(error, result){
+        console.log("save");
+        console.log(result);
+        console.log(error);
+    });
+
+    const fecthMovies = Movie.find({title: "Wild Wild West"}, function(error, result) {
+        console.log("find");
+        console.log(result);
+        console.log(error);
+    });
+    
+});
