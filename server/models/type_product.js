280a17074b55dcaa45def035a5ac54f503e4656c
romainbor <bordier.romain@yahoo.fr>
2019-02-12 15:50:03 +0100

    add type product & product

---
+++ server/models/type_product.js
@@ -0,0 +1,17 @@
+const db = require('../libs/db');
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const type_productSchema = new Schema({
+    name: String,
+    photo: String,
+});
+
+type_productSchema.pre('save', function(next) {
+	if(this.isNew) this.created_at = Date.now();
+	this.updated_at = Date.now();
+	next();
+});
+
+module.exports = db.model('Type_Product', bakerySchema);
